df70d54e5778462c4d34011a9e2d13102742fe14
sdumbre1 <sdumbre@charlotte.edu>
2024-06-15 12:02:06 +0530

    Done Precourse2

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,22 +2,41 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+  #write your code here
+    pivot = arr[low] #Assign the first element as pivot element
+    i = low # i will search for elements greater than pivot element
+    j = high # j will search for elements smaller than pivot element
+    while(i<=j):
+        print(arr)
+        while True:
+            i+=1 #increment i till ith element is greater than pivot or until i reached the high pointer
+            if i>=high or arr[i]>=pivot:
+                break
+        while True:
+            j-=1 #decrement j until jth element is less than pivot or until j reached the low pointer
+            if j<=low or arr[j]<pivot:
+                break
+        if (i<=j):
+            (arr[i],arr[j])=(arr[j],arr[i]) #swap the ith and jth element, so that all the smaller elements are to the left of pivot and bigger element are to right of pivot
+    (arr[low],arr[j])=(arr[j],arr[low]) # to place the pivot in right positionin array
+    return j
+
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
     #write your code here
-  
+    if (low<high):
+        j = partition(arr,low,high)
+        quickSort(arr,low,j) #here the element at the jth position will act as infinity
+        quickSort(arr,j+1,high)
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
+arr.append(9999) #add large element to the end of array depicting infinity
 n = len(arr) 
-quickSort(arr,0,n-1) 
+quickSort(arr,0,n-1)
+arr.pop() #pop the infinity element
 print ("Sorted array is:") 
-for i in range(n): 
+for i in range(len(arr)): 
     print ("%d" %arr[i]), 
   
  
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,19 +3,31 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data
+        self.next = None
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = None
   
     def push(self, new_data): 
-        
-  
+        if (self.head == None):
+           self.head = Node(new_data) 
+        else:
+            curr = self.head
+            while(curr.next is not None):
+                curr = curr.next
+            curr.next = Node(new_data)
+        print(new_data)
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
-
+        fast_ptr = self.head
+        slow_ptr = self.head
+        while(fast_ptr!=None and fast_ptr.next!=None):
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+        print("Middle element :",slow_ptr.data)
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
